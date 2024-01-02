3aa9f7a3af65d3b61696984ed3f15b5339ce93a9
timfuster <tim.fuster4@gmail.com>
2024-01-02 12:08:29 +0100

    Initialer Commit

---
+++ Backend/index.js
@@ -0,0 +1,125 @@
+import express from "express"
+import mysql from "mysql"
+import cors from "cors"
+
+const app = express()
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "projectdb"
+})
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req,res)=>{
+    res.json("Hello this is Backend")
+})
+
+app.get("/materials", (req,res)=>{
+    const q = "SELECT * FROM materials"
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.post("/materials", (req,res)=>{
+    const q = "INSERT INTO materials (`image`, `title`, `description`, `ISBN`, `Kommentar`, `Anzahl`, `Ausgeliehen`, `Kategorie`, `Fach`, `Klassenstufe`, `Regal`, `AktuellePosition`, `link`) VALUES (?)"
+    const values = [
+      req.body.image,
+      req.body.title,
+      req.body.description, 
+      req.body.ISBN, 
+      req.body.Kommentar, 
+      req.body.Anzahl,
+      req.body.Ausgeliehen, 
+      req.body.Kategorie, 
+      req.body.Fach, 
+      req.body.Klassenstufe,
+      req.body.Regal,
+      req.body.AktuellePosition,
+      req.body.link
+    ];
+
+    db.query(q,[values],(err,data)=>{
+        if(err) return res.json(err)
+        return res.json("Material has been created")
+    })
+})
+
+app.delete("/materials/:id", (req, res) => {
+    const materialId = req.params.id;
+    const q = "DELETE FROM materials WHERE id = ?";
+    db.query(q, [materialId], (err, data) => {
+        if (err) return res.json(err);
+        return res.json("Material has been deleted");
+    });
+});
+
+app.get("/materials/:id", (req, res) => {
+    const materialId = req.params.id;
+    const q = "SELECT * FROM materials WHERE id = ?";
+    db.query(q, [materialId], (err, data) => {
+        if (err) return res.json(err);
+        return res.json(data[0]);
+    });
+})
+
+app.get("/countSchnellregal", (req, res) => {
+    const q = "SELECT COUNT(*) AS schnellregalCount FROM materials WHERE AktuellePosition = 'Schnellregal'";
+    db.query(q, (err, data) => {
+        if (err) return res.json(err);
+        return res.json(data[0]);
+    })
+})
+
+app.get("/teachers/:materialId", (req, res) => {
+    const materialId = req.params.materialId;
+
+    // Hier nehmen wir an, dass deine Tabelle mit ausgeliehenen Materialien "borrowed_materials" heißt.
+    const q = `
+        SELECT lehrer.*
+        FROM lehrer
+        JOIN ausgeliehen ON lehrer.ID = ausgeliehen.lehrer_id
+        WHERE ausgeliehen.material_id = ?;
+    `;
+
+    db.query(q, [materialId], (err, data) => {
+        if (err) return res.json(err);
+        return res.json(data);
+    });
+});
+
+app.get("/schnellregal", (req, res) => {
+    const q = "SELECT * FROM materials WHERE AktuellePosition = 'Schnellregal'";
+    db.query(q, (err, data) => {
+        if (err) return res.json(err);
+        return res.json(data);
+    })
+})
+
+app.get("/schnellregal/:id", (req, res) => {
+    const materialId = req.params.id;
+    const q = "SELECT * FROM materials WHERE id = ?";
+    db.query(q, [materialId], (err, data) => {
+        if (err) return res.json(err);
+        return res.json(data[0]);
+    });
+})
+
+app.get("/schnellregalItem/:id", (req, res) => {
+    const materialId = req.params.id;
+    const q = "SELECT * FROM ausgeliehen WHERE material_id = ?";
+    db.query(q, [materialId], (err, data) => {
+        if (err) return res.json(err);
+        return res.json(data[0]);
+    });
+})
+
+
+app.listen(8800, ()=>{
+    console.log("Connected to backend!")
+})
---
+++ Material.session.sql
@@ -0,0 +1,3 @@
+ALTER TABLE materials
+ADD COLUMN link TEXT;
+
